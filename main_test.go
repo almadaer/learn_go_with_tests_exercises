60a749c456d3cf68b1e7d156f280f017ef5d39b1
almadaer <almadaer@gmail.com>
2021-08-11 17:38:08 +0300

    DONE Hello, World

--- main_test.go
+++ main_test.go
@@ -18,14 +18,34 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
+
 	t.Run("say 'Hello, World' when an empty string is supploed'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francois", "French")
+		want := "Bonjour, Francois"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Greta", "German")
+		want := "Hallo, Greta"
 		assertCorrectMessage(t, got, want)
 	})
 }
