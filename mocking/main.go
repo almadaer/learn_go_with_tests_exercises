ddbd44a54fa07131684b137e3fc348da668f3259
almadaer <almadaer@gmail.com>
2021-08-14 16:12:57 +0300

    kinda DONE mocking

---
+++ mocking/main.go
@@ -0,0 +1,37 @@
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking
+// TODO: return later
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
